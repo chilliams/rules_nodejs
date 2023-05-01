2e12edaad601d62bc9a3b8f0d18542c0a523efb5
Christopher Williams <cwilliams@yext.com>
2023-05-01 17:45:23 -0400

    Get Closure working without rules_closure

--- examples/closure/hello.js
+++ examples/closure/hello.js
@@ -1,4 +1,9 @@
-function hello(name) {
-  alert('Hello, ' + name);
-}
-hello('New user');
+import client from 'react-dom/client';
+import jsx from 'react/jsx-runtime';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = client.createRoot(document.getElementById('app'));
+root.render(jsx.jsx('h1', { children: 'Hello world' }));
